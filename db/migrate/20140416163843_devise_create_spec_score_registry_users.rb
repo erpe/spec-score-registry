5f1695effc1cbc43b99f65bfd9c9dd61816fcf28
Rene Paulokat <rene@so36.net>
2014-04-16 19:37:27 +0200

    adopt migration - check if table is already existent

--- db/migrate/20140416163843_devise_create_spec_score_registry_users.rb
+++ db/migrate/20140416163843_devise_create_spec_score_registry_users.rb
@@ -1,42 +1,63 @@
 class DeviseCreateSpecScoreRegistryUsers < ActiveRecord::Migration
-  def change
-    create_table(:sc_users) do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+  def up
+    if  ActiveRecord::Base.connection.table_exists? 'sc_users'
+      add_column :sc_users, :email, :string, null: false, default: ""
+      add_column :sc_users, :encrypted_password, :string,  null: false, default: ""
+      add_column :sc_users, :reset_password_token, :string
+      add_column :sc_users, :reset_password_token_sent_at, :date
+      add_column :sc_users, :remember_created_at, :date
 
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      add_column :sc_users, :sign_in_count, :integer,  default: 0, null: false
+      add_column :sc_users, :current_sign_in_at, :date
+      add_column :sc_users, :last_sign_in_at, :date
+      add_column :sc_users, :current_sign_in_ip, :string
+      add_column :sc_users, :last_sign_in_ip, :string
+      
+      add_index :sc_users, :email,                unique: true
+      add_index :sc_users, :reset_password_token, unique: true
 
-      ## Rememberable
-      t.datetime :remember_created_at
+    else
+      create_table(:sc_users) do |t|
+        ## Database authenticatable
+        t.string :email,              null: false, default: ""
+        t.string :encrypted_password, null: false, default: ""
 
-      ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+        ## Recoverable
+        t.string   :reset_password_token
+        t.datetime :reset_password_sent_at
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+        ## Rememberable
+        t.datetime :remember_created_at
 
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+        ## Trackable
+        t.integer  :sign_in_count, default: 0, null: false
+        t.datetime :current_sign_in_at
+        t.datetime :last_sign_in_at
+        t.string   :current_sign_in_ip
+        t.string   :last_sign_in_ip
+
+        ## Confirmable
+        # t.string   :confirmation_token
+        # t.datetime :confirmed_at
+        # t.datetime :confirmation_sent_at
+        # t.string   :unconfirmed_email # Only if using reconfirmable
+
+        ## Lockable
+        # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+        # t.string   :unlock_token # Only if unlock strategy is :email or :both
+        # t.datetime :locked_at
 
 
-      t.timestamps
+        t.timestamps
     end
 
     add_index :sc_users, :email,                unique: true
     add_index :sc_users, :reset_password_token, unique: true
     # add_index :spec_score_registry_users, :confirmation_token,   unique: true
     # add_index :spec_score_registry_users, :unlock_token,         unique: true
+  end
+
+  def down
+    puts "would guess this is NOT what you actually would like to happen..."
   end
 end
